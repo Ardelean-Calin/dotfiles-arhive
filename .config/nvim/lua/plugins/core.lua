f4c10d2753e2a07f1eff3fc8f884aee00c3208e6
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-07-13 09:33:50 +0300

    Added rust inlay hints and removed plugin update notifications.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,9 +1,34 @@
 return {
   {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      tools = {
+        inlay_hints = {
+          auto = true,
+          only_current_line = false,
+          show_parameter_hints = true,
+        },
+      },
+    },
+  },
+  {
     "echasnovski/mini.indentscope",
     opts = {
       draw = {
         animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "stylua",
+        "nil",
+        "shfmt",
+        "bash-language-server",
+        "lua-language-server",
       },
     },
   },
