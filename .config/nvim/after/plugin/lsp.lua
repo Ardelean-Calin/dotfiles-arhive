23522fd3f9c5863acf83bcafc66dc3171a9775e8
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-08-08 14:58:57 +0300

    Added new LSP shortcut.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -34,6 +34,7 @@ local on_attach = function(client, bufnr)
   bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
 
   bufmap("K", vim.lsp.buf.hover)
+  vim.keymap.set("i", "<CTRL>K", vim.lsp.buf.hover)
 
   vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
     lsp_formatting(bufnr)
